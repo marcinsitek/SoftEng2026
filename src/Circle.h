d3bb129964628e553a019d504e498319ef22bcc7
Bartosz Banaszek <129880@uksw.edu.pl>
2026-03-08 11:00:34 +0100

    zmiana

--- src/Circle.h
+++ src/Circle.h
@@ -1,56 +1,55 @@
-#ifndef _CIRCLE_H
-#define _CIRCLE_H
+#ifndef CIRCLE_H_
+#define CIRCLE_H_
 
 #include "Shape2D.h"
-#include "ShapeResultData.h"
 #include "ShapeParam.h"
-#include <string>
-#include <sstream>
+#include "ShapeResultData.h"
 
-template<class T>
+#include <sstream>
+#include <string>
+
+template <class T>
 class Circle : public Shape2D<T> {
 public:
-    inline ShapeResultData<T> compute();
+    inline ShapeResult<T> compute();
     inline std::string print();
     inline Circle(const ShapeParam<T>& param);
-}
+};
 
-template<class T>
-inline ShapeResultData<T> Circle<T>::compute() {
-    T radius = this->m_param.get(PARAM_RADIUS);
+template <class T>
+inline ShapeResult<T> Circle<T>::compute() {
+    T radius = this->m_param.get_attrib(PARAM_RADIUS);
     const double PI = 3.14159265358979323846;
-    
+
     // Obliczenia na typie double dla precyzji
     double area = PI * static_cast<double>(radius) * static_cast<double>(radius);
     double perimeter = 2.0 * PI * static_cast<double>(radius);
 
     // Pakowanie wynikow do obiektu ShapeResultData
-    ShapeResultData<T> result;
-    result.set(RESULT_AREA, static_cast<T>(area));
-    result.set(RESULT_PERIMETER, static_cast<T>(perimeter));
+    ShapeResult<T> result;
+    result.set_attrib(RESULT_AREA, static_cast<T>(area));
+    result.set_attrib(RESULT_PERIMETER, static_cast<T>(perimeter));
 
     return result;
 }
 
-template<class T>
+template <class T>
 inline std::string Circle<T>::print() {
-    T radius = this->m_param.get(PARAM_RADIUS);
-    ShapeResultData<T> result = compute();
+    T radius = this->m_param.get_attrib(PARAM_RADIUS);
+    ShapeResult<T> result = compute();
 
     // Uzycie ostringstream do zbudowania wieloliniowego tekstu
     std::ostringstream out;
     out << "=== FIGURA: KOLO ===" << std::endl;
     out << "Promien: " << radius << std::endl;
-    out << "Pole powierzchni: " << result.get(RESULT_AREA) << std::endl;
-    out << "Obwod: " << result.get(RESULT_PERIMETER) << std::endl;
+    out << "Pole powierzchni: " << result.get_attrib(RESULT_AREA) << std::endl;
+    out << "Obwod: " << result.get_attrib(RESULT_PERIMETER) << std::endl;
     out << "====================";
 
     return out.str();
 }
 
-template<class T>
-inline Circle<T>::Circle(const ShapeParam<T>& param) : Shape2D<T>(param) {
-}
+template <class T>
+inline Circle<T>::Circle(const ShapeParam<T>& param) : Shape2D<T>(param) {}
 
-#endif
-
+#endif // CIRCLE_H_
