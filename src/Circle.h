355d5dfea8e5152af8b751a6c1cd3c725212b1c9
Bartek K <bkasicki754@gmail.com>
2026-03-19 16:25:31 +0100

    Poprawa 5

--- src/Circle.h
+++ src/Circle.h
@@ -1,38 +1,31 @@
-#ifndef _CIRCLE_H
-#define _CIRCLE_H
+#ifndef CIRCLE_H_
+#define CIRCLE_H_
 
 #include "Shape2D.h"
-#include "ShapeResultData.h"
 #include "ShapeParam.h"
-#include "ShapeResultIndex.h"
-#include <string>
+#include "ShapeResultData.h"
+
 #include <sstream>
-#include <stdexcept>
+#include <string>
 
 template <class T> class Circle : public Shape2D<T> {
 public:
-    // Definicje metod wewn¹trz klasy
-    ShapeResult<T> compute();
-    std::string print();
-    Circle(const ShapeParam<T>& param);
+    inline ShapeResult<T> compute();
+    inline std::string print();
+    inline Circle(const ShapeParam<T>& param);
 };
 
-// Implementacja metody compute
-template <class T> ShapeResult<T> Circle<T>::compute()
+template <class T> inline ShapeResult<T> Circle<T>::compute()
 {
-    // this->m_param pochodzi z klasy bazowej Shape2D
     T radius = this->m_param.get_attrib(PARAM_RADIUS);
     const double PI = 3.14159265358979323846;
 
-    if (radius < static_cast<T>(0))
-    {
-        throw std::invalid_argument("Promien kola nie moze byc ujemny!");
-    }
-
+    // Obliczenia na typie double dla precyzji
     double area =
         PI * static_cast<double>(radius) * static_cast<double>(radius);
     double perimeter = 2.0 * PI * static_cast<double>(radius);
 
+    // Pakowanie wynikow do obiektu ShapeResultData
     ShapeResult<T> result;
     result.set_attrib(RESULT_AREA, static_cast<T>(area));
     result.set_attrib(RESULT_PERIMETER, static_cast<T>(perimeter));
@@ -40,13 +33,12 @@ template <class T> ShapeResult<T> Circle<T>::compute()
     return result;
 }
 
-// Implementacja metody print
-template <class T> std::string Circle<T>::print()
+template <class T> inline std::string Circle<T>::print()
 {
     T radius = this->m_param.get_attrib(PARAM_RADIUS);
-    // Tutaj byla literowka w typie danych - teraz jest ShapeResult
     ShapeResult<T> result = compute();
 
+    // Uzycie ostringstream do zbudowania wieloliniowego tekstu
     std::ostringstream out;
     out << "=== FIGURA: KOLO ===" << std::endl;
     out << "Promien: " << radius << std::endl;
@@ -57,10 +49,8 @@ template <class T> std::string Circle<T>::print()
     return out.str();
 }
 
-// Konstruktor
 template <class T>
-Circle<T>::Circle(const ShapeParam<T>& param): Shape2D<T>(param)
-{
-}
+inline Circle<T>::Circle(const ShapeParam<T>& param): Shape2D<T>(param)
+{}
 
-#endif
+#endif // CIRCLE_H_
